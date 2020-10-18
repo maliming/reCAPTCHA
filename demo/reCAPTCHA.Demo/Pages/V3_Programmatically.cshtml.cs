f64fc5c5e14087f84d5e7dc12c40037be8028cb7
maliming <6908465+maliming@users.noreply.github.com>
2020-10-18 17:17:57 +0800

    Support reCAPTCHA v3 programmatically.
    Fix #4

---
+++ demo/reCAPTCHA.Demo/Pages/V3_Programmatically.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Owl.reCAPTCHA;
+using Owl.reCAPTCHA.v3;
+
+namespace reCAPTCHA.Demo.Pages
+{
+    public class V3_ProgrammaticallyModel : PageModel
+    {
+        private readonly IreCAPTCHASiteVerifyV3 _siteVerify;
+
+        public string Result { get; set; }
+
+        public V3_ProgrammaticallyModel(IreCAPTCHASiteVerifyV3 siteVerify)
+        {
+            _siteVerify = siteVerify;
+        }
+
+        public async Task OnPostAsync(string token)
+        {
+            var response = await _siteVerify.Verify(new reCAPTCHASiteVerifyRequest
+            {
+                Response = token,
+                RemoteIp = HttpContext.Connection.RemoteIpAddress.ToString()
+            });
+
+            Result = JsonConvert.SerializeObject(response, Formatting.Indented);
+        }
+    }
+}
--- src/Owl.reCAPTCHA/v3/TagHelpers/reCAPTCHAV3ScriptJsTagHelper.cs
+++ src/Owl.reCAPTCHA/v3/TagHelpers/reCAPTCHAV3ScriptJsTagHelper.cs
@@ -10,37 +10,50 @@ namespace Owl.reCAPTCHA.v3.TagHelpers
 
         public string Callback { get; set; }
 
+        public bool Execute { get; set; }
+
         private readonly reCAPTCHAOptions _options;
 
         public reCAPTCHAV3ScriptJsTagHelper(IOptionsSnapshot<reCAPTCHAOptions> optionsAccessor)
         {
             _options = optionsAccessor.Get(reCAPTCHAConsts.V3);
+            Execute = true;
         }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             /*
-            grecaptcha.ready(function() {
-                grecaptcha.reExecute = function(){
-                    grecaptcha.execute('_reCAPTCHA_site_key_', {action: 'homepage'}).then(function(token) {
-                   ...
-                })();
-                }
+            myCallback is a user-defined method name or `(function(t){alert(t)})` when Execute = true
+            grecaptcha.ready(function () {
+                grecaptcha.reExecute = function () {
+                    grecaptcha.execute('6LccrsMUAAAAANSAh_MCplqdS9AJVPihyzmbPqWa', {
+                        action: 'login'
+                    }).then(function (token) {
+                        myCallback(token)
+                    })
+                };
+                grecaptcha.reExecute()
             });
-            */
 
+            myCallback is a user-defined function when Execute = false
+            grecaptcha.ready(function () {
+                grecaptcha.reExecute = function (callback) {
+                    grecaptcha.execute('6LccrsMUAAAAANSAh_MCplqdS9AJVPihyzmbPqWa', {
+                        action: 'login'
+                    }).then(myCallback)
+                };
+            });
+             */
             output.TagName = "script";
             output.TagMode = TagMode.StartTagAndEndTag;
 
-            var script = "grecaptcha.ready(function(){ " +
-                         "grecaptcha.reExecute = function(){" +
-                             "grecaptcha.execute('" + _options.SiteKey + "'" + (string.IsNullOrWhiteSpace(Action) ? "" : ",{action:'" + Action + "'}") + ")" +
-                                 ".then(function(token){" +
-                                    Callback + "(token)" +
-                                 "})" +
-                            "};" +
-                           "grecaptcha.reExecute()" +
-                         "});";
+            var script =
+                "grecaptcha.ready(function(){ " +
+                     "grecaptcha.reExecute = function(" + (Execute ? "" : "callback") + "){" +
+                        "grecaptcha.execute('" + _options.SiteKey + "'" + (string.IsNullOrWhiteSpace(Action) ? "" : ",{action:'" + Action + "'}") + ")" +
+                            (Execute ? (".then(function(token){" + Callback + "(token)" + "})") : ".then(callback)") + "};" +
+                            (Execute ? "grecaptcha.reExecute()" :"") +
+                "});";
             output.Content.SetHtmlContent(script);
         }
     }
